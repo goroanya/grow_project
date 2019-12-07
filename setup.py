740700109a1ed83181eaa3993d18078984925abc
Ihor Bulaiesvkyi <latente.traumgedanken@gmail.com>
2019-12-07 22:28:27 +0200

    project structure created

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='Department-app',
+      version='1.0',
+      description='Python Distribution Utilities',
+      author='Ihor Bulaievskyi',
+      author_email='ihorbulaievskyi@gmail.com',
+      url='https://github.com/traumgedanken/grow_project'
+      )
