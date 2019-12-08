9bee00c9bfce3f7420c1ea6db048fa6732b8a6a7
Ihor Bulaiesvkyi <latente.traumgedanken@gmail.com>
2019-12-08 12:31:45 +0200

    project build with travis

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(name='Department-app',
       version='1.0',
-      description='Python Distribution Utilities',
+      description='A simple web application for managing departments and employees',
       author='Ihor Bulaievskyi',
       author_email='ihorbulaievskyi@gmail.com',
       url='https://github.com/traumgedanken/grow_project'
