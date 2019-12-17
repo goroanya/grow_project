819635b1b2aeeca386125117b540316cd565c218
Ihor Bulaiesvkyi <latente.traumgedanken@gmail.com>
2019-12-17 23:47:02 +0200

    config.py upd

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import socket
 import os
 import getpass
+import sys
+import subprocess
 
 # Cleaning for not the first run
 os.system('systemctl kill server')
@@ -10,15 +12,15 @@ FILES_TO_DELETE = ['server.sock',
                    '/etc/systemd/system/server.service',
                    '/etc/systemd/system/client.service',
                    '/etc/nginx/sites-available/server',
-                   '/etc/nginx/sites-available/client',
-                   '/etc/nginx/sites-enabled/server',
-                   'etc/nginx/sites-enabled/client']
+                   '/etc/nginx/sites-available/client']
 for file in FILES_TO_DELETE:
     try:
         os.remove(file)
     except OSError:
         pass
 
+os.system('sudo unlink /etc/nginx/sites-enabled/server')
+os.system('sudo unlink /etc/nginx/sites-enabled/client')
 SERVICE_DESCRIPTION = '''
 [Unit]
 Description=$DESCRIPTION
@@ -28,8 +30,8 @@ After=network.target
 User=$USERNAME
 Group=www-data
 WorkingDirectory=$CWD
-Environment="PATH=$CWD/venv/bin"
-ExecStart=$CWD/venv/bin/gunicorn --log-file $NAME.log --log-level debug --bind unix:$NAME.sock -m 007 $MODULE_NAME:APP
+Environment="PATH=$EXECUTABLE"
+ExecStart=$GUNICORN_PATH --log-file $CWD/$NAME.log --log-level debug --bind unix:$CWD/$NAME.sock -m 007 $MODULE_NAME:APP
 
 [Install]
 WantedBy=multi-user.target'''
@@ -38,30 +40,33 @@ SERVER_SOCKET_PATH = 'server.sock'
 CLIENT_SOCKET_PATH = 'client.sock'
 USERNAME = getpass.getuser()
 CWD = os.getcwd()
+EXECUTABLE_PYTHON_DIR = '/'.join(sys.executable.split('/')[:-1])
+GUNICORN_PATH = subprocess.check_output(['whereis', 'gunicorn']).decode('utf-8').split(' ')[1].strip()
 
 # Creating sockets
 socket.socket(socket.AF_UNIX).bind(SERVER_SOCKET_PATH)
 socket.socket(socket.AF_UNIX).bind(CLIENT_SOCKET_PATH)
 print('Sockets created')
 
-with open('/etc/systemd/system/server.service', 'w') as f:
-    data = SERVICE_DESCRIPTION \
-        .replace('$USERNAME', USERNAME) \
-        .replace('$CWD', CWD) \
-        .replace('$DESCRIPTION', 'Server service') \
-        .replace('$NAME', 'server') \
-        .replace('$MODULE_NAME', 'rest')
-    f.write(data)
+data = SERVICE_DESCRIPTION \
+    .replace('$USERNAME', USERNAME) \
+    .replace('$GUNICORN_PATH', GUNICORN_PATH) \
+    .replace('$EXECUTABLE', EXECUTABLE_PYTHON_DIR) \
+    .replace('$CWD', CWD) \
+    .replace('$DESCRIPTION', 'Server service') \
+    .replace('$NAME', 'server') \
+    .replace('$MODULE_NAME', 'rest')
+os.system(f'sudo sh -c \'printf "{data}" > /etc/systemd/system/server.service\'')
 
-with open('/etc/systemd/system/client.service', 'w') as f:
-    data = SERVICE_DESCRIPTION \
+data = SERVICE_DESCRIPTION \
         .replace('$USERNAME', USERNAME) \
+        .replace('$GUNICORN_PATH', GUNICORN_PATH) \
+        .replace('$EXECUTABLE', EXECUTABLE_PYTHON_DIR) \
         .replace('$CWD', CWD) \
         .replace('$DESCRIPTION', 'Client service') \
         .replace('$NAME', 'client') \
         .replace('$MODULE_NAME', 'views')
-    f.write(data)
-
+os.system(f'sudo sh -c \'printf "{data}" > /etc/systemd/system/client.service\'')
 print('Service information written')
 
 NGINX_DESCRIPTION = '''
@@ -73,39 +78,35 @@ server {
     server_name $NAME.test;
 
     location / {
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header Host $http_host;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header Host \$http_host;
         proxy_redirect off;
-        if (!-f $request_filename) {
+        if (!-f \$request_filename) {
             proxy_pass http://$NAME;
             break;
         }
     }
     location /static/ {
         root $CWD/;
-        try_files $uri $uri/ =404;
+        try_files \$uri \$uri/ =404;
     }
 }
 '''
 
-with open('/etc/nginx/sites-available/server', 'w') as f:
-    data = NGINX_DESCRIPTION.replace('$CWD', CWD).replace('$NAME', 'server')
-    f.write(data)
+data = NGINX_DESCRIPTION.replace('$CWD', CWD).replace('$NAME', 'server')
+os.system(f'sudo sh -c \'printf "{data}" > /etc/nginx/sites-available/server\'')
+data = NGINX_DESCRIPTION.replace('$CWD', CWD).replace('$NAME', 'client')
+os.system(f'sudo sh -c \'printf "{data}" > /etc/nginx/sites-available/client\'')
 
-with open('/etc/nginx/sites-available/client', 'w') as f:
-    data = NGINX_DESCRIPTION.replace('$CWD', CWD).replace('$NAME', 'client')
-    f.write(data)
-
-HOSTS_LINE = '127.0.1.1   client.test server.test'
-with open('/etc/hosts', 'a+') as f:
+HOSTS_LINE = '127.0.1.1 client.test server.test'
+with open('/etc/hosts', 'r') as f:
     data = f.read()
     if HOSTS_LINE not in data:
-        f.write('\n')
-        f.write(HOSTS_LINE)
+        os.system(f'sudo sh -c \'printf "\n{HOSTS_LINE}\n" >>  /etc/hosts\'')
 
 print('NGINX information written')
-print('Starting services...')
-os.system('ln -s /etc/nginx/sites-available/server /etc/nginx/sites-enabled')
-os.system('ln -s /etc/nginx/sites-available/client /etc/nginx/sites-enabled')
+os.system('sudo ln -s /etc/nginx/sites-available/server /etc/nginx/sites-enabled')
+os.system('sudo ln -s /etc/nginx/sites-available/client /etc/nginx/sites-enabled')
 os.system('systemctl daemon-reload')
 os.system('systemctl restart nginx')
+print('Nginx service are configured')
--- setup.sh
+++ setup.sh
@@ -1,7 +1,5 @@
 #?/bin/bash
 
-rm -rf venv
 sudo apt-get install -y virtualenv nginx
-virtualenv -p python3.8 venv
 pip install -r requirements.txt
 sudo python config.py
